818f4203a9fad12517aba4cc14dc6a2d78b6f2ce
Yousif Kndkji <yousif.kndkji@gmail.com>
2024-11-19 21:41:19 -0500

    add jsconfig.json for alias resolve and implement debounced query for search bar

--- client/src/components/RecipeSearchEngine.jsx
+++ client/src/components/RecipeSearchEngine.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+
+/*
+{
+	dietary-restriction1: [ingredient1, ingredient2, ...],
+	...: [...]
+}
+*/
+
 function RecipeSearchEngine() {
+	const [query, setQuery] = useState("");
+
+	useEffect(() => console.log(query), [query]);
+
 	return (
-		<input type="text" placeholder="Search for your next recipe..."></input>
+		<input
+			type="text"
+			value={query}
+			onChange={(e) => setQuery(e.target.value)}
+			placeholder="Search for your next recipe..."
+		></input>
 	);
 }
 
