ca9648ff29af1a583f03200829a42bc56c3435c6
Yousif Kndkji <yousif.kndkji@gmail.com>
2024-11-21 23:51:55 -0500

    finish main functionality of search results view and added pagination

--- client/src/components/RecipeSearchResultView.jsx
+++ client/src/components/RecipeSearchResultView.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react";
 
 import { getRecipeQuery } from "@/api/recipeApi";
 
+import "@/styles/SearchInterface.css";
+
 function RecipeSearchResultView({ query }) {
-	const [searchResults, setSearchResults] = useState([]);
+	const [searchResults, setSearchResults] = useState({});
 	const [searchLoading, setSearchLoading] = useState(false);
 	const [searchError, setSearchError] = useState(null);
 
 	useEffect(() => {
 		if (query.length === 0) {
-			setSearchResults([]);
+			setSearchResults({});
 			return;
 		}
 
@@ -19,7 +21,7 @@ function RecipeSearchResultView({ query }) {
 
 			try {
 				const res = await getRecipeQuery(query);
-				setSearchResults(res.data.meals);
+				setSearchResults(res.data);
 			} catch (err) {
 				setSearchError(`Recipe fetch failed (Status Code: ${err.response.status} `);
 			} finally {
@@ -34,13 +36,28 @@ function RecipeSearchResultView({ query }) {
 		<div>
 			{searchLoading && <p>Loading...</p>}
 			{searchError && <p style={{ color: "red" }}>{searchError}</p>}
-			<p>{searchResults.length > 0 && searchResults[0].strMeal}</p>
-			{/* <ul>
-				{data.split("").map((char, i) => {
-					console.log(`${char} re-render?`);
-					return <li key={i}>{char}</li>;
-				})}
-			</ul> */}
+			{searchResults.meals?.length > 0 && (
+				<div className="search-interface-results-container">
+					{searchResults.meals.map(meal => (
+						<div key={meal.idMeal} className="search-interface-result-item">
+							<div className="search-interface-food-name">{meal.strMeal}</div>
+							<div className="search-interface-food-category">Category: {meal.strCategory}</div>
+						</div>
+					))}
+					{searchResults.paginationData && (
+						<div className="search-interface-result-info">
+							<div className="search-interface-navigation">
+								{/* // TODO: Add onClick event that checks cache for previous or next page
+									and if it doesnt exist, then make a new api request for previous or next page
+									then add it to cache (probably localStorage or redis, not sure yet) */}
+								<button className="search-interface-nav-button">&#10094;</button>
+								{searchResults.paginationData.currentPage + 1} / {searchResults.paginationData.totalPages}
+								<button className="search-interface-nav-button">&#10095;</button>
+							</div>
+						</div>
+					)}
+				</div>
+			)}
 		</div>
 	);
 }
--- server/src/routes/api/recipes.js
+++ server/src/routes/api/recipes.js
@@ -5,14 +5,35 @@ const recipeApi = axios.create({ baseURL: "https://www.themealdb.com/api/json/v1
 
 const router = Router();
 
-// router.use("/", (req, res, next) => {
-// 	res.paginate = data => {
-// 		// TODO: implement function that paginates json
-// 		return data;
-// 	};
+router.use("/", (req, res, next) => {
+	res.paginate = data => {
+		const displayLimit = parseInt(req.query.limit) || 3;
+		if (data.meals.length > displayLimit) {
+			const page = parseInt(req.query.page) || 1;
 
-// 	next();
-// });
+			const start = (page - 1) * displayLimit;
+			const end = page * displayLimit;
+
+			data.meals = data.meals.slice(start, end);
+			data.paginationData = {
+				currentPage: page,
+				totalPages: Math.ceil(data.meals.length),
+			};
+		}
+		/*
+		 * {
+		 *		meals: [...],
+		 *		paginationData: {
+		 *			currentPage,
+		 *			totalPages
+		 *		}
+		 * }
+		 */
+		return data;
+	};
+
+	next();
+});
 
 router.route("/").get(async (req, res) => {
 	const recipeResponse = await recipeApi.request({
@@ -22,8 +43,8 @@ router.route("/").get(async (req, res) => {
 		},
 	});
 
-	// const response = res.paginate(recipeResponse.data);
-	res.status(200).json(recipeResponse.data);
+	const paginatedData = res.paginate(recipeResponse.data);
+	res.status(200).json(paginatedData);
 });
 
 router.route("/:query").get(async (req, res) => {
@@ -34,9 +55,8 @@ router.route("/:query").get(async (req, res) => {
 		},
 	});
 
-	// const response = res.paginate(recipeResponse.data);
-	console.log(JSON.stringify(recipeResponse.data));
-	res.status(200).json(recipeResponse.data);
+	const paginatedData = res.paginate(recipeResponse.data);
+	res.status(200).json(paginatedData);
 });
 
 export default router;
