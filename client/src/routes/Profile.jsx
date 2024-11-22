67595a46642e4cb36a9fc1d6e4190bd075f45c6c
Yousif Kndkji <yousif.kndkji@gmail.com>
2024-11-21 21:30:52 -0500

    more tidying

--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -1,19 +1,14 @@
 const Profile = () => {
-
 	return (
-		
-       <div className="User Login"><div>
-       <input type="text" placeholder="Username"></input>
-       </div>
-   <div>
-       <input type="text" placeholder="Password"></input>
-   
-   </div>
-   </div> 
-        
-        
+		<div className="User Login">
+			<div>
+				<input type="text" placeholder="Username"></input>
+			</div>
+			<div>
+				<input type="text" placeholder="Password"></input>
+			</div>
+		</div>
 	);
-}
-
+};
 
 export default Profile;
--- client/src/routes/RecipesDatabase.jsx
+++ client/src/routes/RecipesDatabase.jsx
@@ -1,9 +1,5 @@
 const RecipesDatabase = () => {
-
-	return (
-		<input type="text" placeholder="hey..."></input>
-	);
-}
-
+	return <input type="text" placeholder="hey..."></input>;
+};
 
 export default RecipesDatabase;
