51f9d0ddc99914be9f5be8e24684effef31aaf4b
Yousif Kndkji <yousif.kndkji@gmail.com>
2024-12-01 05:10:00 -0500

    Revert "create sprint2 directory, add .env.example, and changed installation guide in README"

--- doc/sprint2/server/src/models/Recipe.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const recipeSchema = new Schema({
-	name: String,
-	description: String,
-	createdAt: { type: Date, default: Date.now },
-});
-
-export default model("Recipe", recipeSchema);
